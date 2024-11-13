0c5e9f60fd194c962fd69e60814665dc6f356d9b
Decadence <enesdgnr666@gmail.com>
2024-11-13 23:52:09 +0300

    Added matrice randomizer

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Operations calc = new Operations();
-        //let A[2,3] B[3,4] -> C[2,4]
-        int[][] A = {{1, 2, 3}, {3, 4, 5}};
-        int[][] B = {{1, 0, 2, 5}, {6, 7, 2, 8}, {2, 2, 1, 9}};
-        System.out.println(Arrays.deepToString(calc.matrixMult(A, B)));
+        int[][] A = calc.randomMatrice(sc, 'A');
+        int[][] B = calc.randomMatrice(sc, 'B');
+        int[][] C = calc.randomMatrice(sc, 'C');
+        int[][] AxB = calc.matrixMult(A, B);
+        System.out.println(Arrays.deepToString(calc.matrixMult(AxB, C)));
     }
 }
--- src/Operations.java
+++ src/Operations.java
@@ -1,6 +1,9 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Operations {
     public int[][] matrixMult(int[][] a, int[][] b) {
-        int[][] res = new int[2][4];
+        int[][] res = new int[a.length][b[0].length];
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < b[0].length; j++) {
                 //res[i][j] = 0;
@@ -10,5 +13,30 @@ public class Operations {
             }
         }
         return res;
+    }
+
+    public int[][] randomMatrice(Scanner sc, char name) {
+        System.out.println("Enter how many rows you want:");
+        int x = sc.nextInt();
+        System.out.println("Enter how many columns you want:");
+        int y = sc.nextInt();
+        int[][] matrice = new int[x][y];
+        Random random = new Random();
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                matrice[i][j] = random.nextInt(10); // Generates random integers between 0 and 99
+            }
+        }
+
+        // Print the matrice
+        System.out.println("Matrice " + name + ":");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print(matrice[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return matrice;
     }
 }
