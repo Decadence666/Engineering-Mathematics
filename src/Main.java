03d51d863a9392775dfef5bbed2ed3001ecdbb68
Decadence <enesdgnr666@gmail.com>
2024-11-14 01:54:28 +0300

    added matrix sum, added boolean operations

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Operations calc = new Operations();
-        int[][] A = calc.randomMatrice(sc, 'A');
-        int[][] B = calc.randomMatrice(sc, 'B');
-        int[][] C = calc.randomMatrice(sc, 'C');
+        int[][] sumA = calc.randomMatrix(5, 5, 'A');
+        int[][] sumB = calc.randomMatrix(5, 5, 'B');
+        System.out.println(Arrays.deepToString(calc.matrixSum(sumA, sumB)));
+        int[][] A = calc.randomMatrix(20,30, 'A');
+        int[][] B = calc.randomMatrix(30,40, 'B');
+        int[][] C = calc.randomMatrix(40,10 , 'C');
         int[][] AxB = calc.matrixMult(A, B);
         System.out.println(Arrays.deepToString(calc.matrixMult(AxB, C)));
+        boolean[][] boolA = calc.randomBoolMat(2,2,'x');
+        boolean[][] boolB = calc.randomBoolMat(2,2,'y');
+        System.out.println(Arrays.deepToString(calc.matrixBoolMult(boolA, boolB)));
+        sc.close();
     }
 }
--- src/Operations.java
+++ src/Operations.java
@@ -2,11 +2,62 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Operations {
+    public boolean[][] matrixAND(boolean[][] a, boolean[][] b) {
+        boolean[][] res = new boolean[a.length][a[0].length];
+        if (a.length != b.length || a[0].length != b[0].length) {
+            return null;
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                res[i][j] = a[i][j] && b[i][j];
+            }
+        }
+        return res;
+    }
+
+    public boolean[][] matrixOR(boolean[][] a, boolean[][] b) {
+        boolean[][] res = new boolean[a.length][a[0].length];
+        if (a.length != b.length || a[0].length != b[0].length) {
+            return null;
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                res[i][j] = a[i][j] || b[i][j];
+            }
+        }
+        return res;
+    }
+
+    public boolean[][] matrixBoolMult(boolean[][] a, boolean[][] b) {
+        boolean[][] res = new boolean[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                res[i][j]=false;
+                for (int k = 0; k < a[0].length; k++) {
+                    res[i][j] |= (a[i][k] && b[k][j]);
+                }
+            }
+        }
+        return res;
+    }
+
+    public int[][] matrixSum(int[][] a, int[][] b) {
+        int[][] res = new int[a.length][a[0].length];
+        if (a.length != b.length || a[0].length != b[0].length) {
+            return null;
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                res[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        return res;
+    }
+
     public int[][] matrixMult(int[][] a, int[][] b) {
         int[][] res = new int[a.length][b[0].length];
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < b[0].length; j++) {
-                //res[i][j] = 0;
                 for (int k = 0; k < a[0].length; k++) {
                     res[i][j] += a[i][k] * b[k][j];
                 }
@@ -15,24 +66,51 @@ public class Operations {
         return res;
     }
 
-    public int[][] randomMatrice(Scanner sc, char name) {
+    public int userRow(Scanner sc) {
         System.out.println("Enter how many rows you want:");
-        int x = sc.nextInt();
-        System.out.println("Enter how many columns you want:");
-        int y = sc.nextInt();
-        int[][] matrice = new int[x][y];
+        return sc.nextInt();
+    }
+
+    public int userCol(Scanner sc) {
+        System.out.println("Enter how many rows you want:");
+        return sc.nextInt();
+    }
+
+    public int[][] randomMatrix(int row, int col, char name) {
+        int[][] matrix = new int[row][col];
         Random random = new Random();
 
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                matrice[i][j] = random.nextInt(10); // Generates random integers between 0 and 99
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrix[i][j] = random.nextInt(10); // Generates random integers between 0 and 10
             }
         }
 
         // Print the matrice
         System.out.println("Matrice " + name + ":");
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return matrix;
+    }
+
+    public boolean[][] randomBoolMat(int row, int col, char name) {
+        boolean[][] matrice = new boolean[row][col];
+        Random random = new Random();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrice[i][j] = random.nextBoolean(); // Generates random bool
+            }
+        }
+
+        // Print the matrice
+        System.out.println("Matrice " + name + ":");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 System.out.print(matrice[i][j] + " ");
             }
             System.out.println();
